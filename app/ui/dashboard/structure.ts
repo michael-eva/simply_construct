de432c5f3dc16aafce9d18737a97f72f77b4d784
galactic-shark <evamichael100@gmail.com>
2023-12-27 09:07:24 +0800

    trying new sidenav

--- app/ui/dashboard/structure.ts
+++ app/ui/dashboard/structure.ts
@@ -12,21 +12,21 @@ const sidebarStructure = [
         title: "Project",
         name: "project",
         parent: true,
-        // icon: "transaksi",
+        icon: "transaksi",
         child: [
             {
                 id: "slab",
                 title: "Slab",
                 name: "project.slab",
                 link: "/dashboard/project/slab",
-                // icon: "dot"
+                icon: "dot"
             },
             {
                 id: "walls",
                 title: "Walls",
                 name: "project.walls",
                 link: "/dashboard/project/walls",
-                // icon: "dot"
+                icon: "dot"
             },
         ]
     },
@@ -35,21 +35,21 @@ const sidebarStructure = [
         title: "Library",
         name: "library",
         parent: true,
-        // icon: "perusahaan",
+        icon: "perusahaan",
         child: [
             {
                 id: "wallLib",
                 title: "Wall",
                 name: "library.wallLib",
                 link: "/dashboard/library/walls",
-                // icon: "dot"
+                icon: "dot"
             },
             {
                 id: "slabLib",
                 title: "Slab",
                 name: "library.slabLib",
                 link: "/dashboard/library/slab",
-                // icon: "dot"
+                icon: "dot"
             },
         ]
     },
@@ -59,7 +59,7 @@ const sidebarStructure = [
         title: "Settings",
         name: "settings",
         parent: true,
-        // icon: "pusatunduhdata",
+        icon: "pusatunduhdata",
         link: "/dashboard/settings"
     },
 ];
