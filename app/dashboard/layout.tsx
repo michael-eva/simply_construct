0b9d6ddea31b60075a7443b80798cbafedeebc4f
galactic-shark <evamichael100@gmail.com>
2023-12-27 09:13:04 +0800

    debugging

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,14 @@
+"use client"
+
 import Sidebar from "../ui/dashboard/sidenav"
+import { useState } from 'react'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    const [sideMenuIsExpand, setSideMenuIsExpand] = useState(true);
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
             <div className="w-full flex-none md:w-64">
-                <Sidebar />
+                <Sidebar setExpand={setSideMenuIsExpand} />
             </div>
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         </div>
