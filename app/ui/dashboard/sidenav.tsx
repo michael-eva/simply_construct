b5ccf837a56a25d6dcea62ec3c2779381a86b93b
galactic-shark <evamichael100@gmail.com>
2023-12-27 09:34:12 +0800

    changing sidenav

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -203,6 +203,7 @@ import "simplebar-react/dist/simplebar.min.css";
 import { sidebarStructure } from "./structure";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 interface SidebarProps {
     setExpand: (value: boolean) => void;
@@ -385,98 +386,94 @@ const Sidebar: FC<SidebarProps> = ({ setExpand }) => {
 
         return (
             <li key={index}>
-                <a
-                    role="button"
-                    tabIndex={0}
-                    id={item.id}
-                    onClick={() => {
-                        if ("child" in item) {
-                            handleToggle(item.name);
-                        } else if ("link" in item) {
-                            handleNavigate(item.name);
-                        }
-                    }}
-                    onKeyDown={(event) => {
-                        const { code } = event;
-                        if (code === "Space") {
-                            if ("child" in item) {
+                {item.child ? (
+                    <div
+                        role="button"
+                        tabIndex={0}
+                        onClick={() => handleToggle(item.name)}
+                        onKeyDown={(event) => {
+                            const { code } = event;
+                            if (code === "Space") {
                                 handleToggle(item.name);
-                            } else if ("link" in item) {
-                                handleNavigate(item.name);
                             }
-                        }
-                    }}
-                    className={[
-                        "group m-0 flex cursor-pointer rounded-lg items-center justify-between h-12 py-0 pr-3 mb-1 focus:outline-none",
-                        recursive === 0 ? "pl-4" : recursive === 1 ? "pl-11" : "pl-16",
-                        activeName === item.name || activeName.split(".")[0] === item.name
-                            ? `text-blue-600 font-semibold ${item.parent ? "bg-blue-200/20 " : "bg-transparent"
-                            }`
-                            : `text-slate-500 ${item.parent && ""}`,
-                        "hover:bg-slate-300/20",
-                        classesActive
-                    ].join(" ")}
-                >
-                    <div className="flex items-center gap-3">
-                        {item.icon ? (
-                            item.icon === "dot" ? (
-                                <div className="h-3 w-3 flex items-center justify-center">
-                                    <div
-                                        className={[
-                                            `${classesActive ? "h-2 w-2" : "h-1 w-1"}`,
-                                            "bg-current rounded-full duration-200"
-                                        ].join(" ")}
-                                    ></div>
+                        }}
+                        className={[
+                            "group m-0 flex cursor-pointer rounded-lg items-center justify-between h-12 py-0 pr-3 mb-1 focus:outline-none",
+                            recursive === 0 ? "pl-4" : recursive === 1 ? "pl-11" : "pl-16",
+                            "hover:bg-gray-300/20",
+                            classesActive
+                        ].join(" ")}
+                    >
+                        <Link href={`/${item.name}`} passHref>
+                            <div className="flex items-center gap-3">
+                                {item.icon ? (
+                                    item.icon === "dot" ? (
+                                        <div className="h-3 w-3 flex items-center justify-center">
+                                            <div
+                                                className={[
+                                                    `${classesActive ? "h-2 w-2" : "h-1 w-1"}`,
+                                                    "bg-current rounded-full transition duration-200"
+                                                ].join(" ")}
+                                            ></div>
+                                        </div>
+                                    ) : (
+                                        generateIcon(item.icon)
+                                    )
+                                ) : null}
+                                <div
+                                    className={`truncate ${isExpand ? "" : isExpandOnHover ? "" : "w-0 h-0 opacity-0"
+                                        }`}
+                                >
+                                    {item.title}
                                 </div>
-                            ) : (
-                                generateIcon(item.icon)
-                            )
-                        ) : null}
+                            </div>
+                        </Link>
+                        {item.child && (
+                            <div
+                                className={`${isExpand ? "" : isExpandOnHover ? "" : "w-0 h-0 opacity-0"
+                                    }`}
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <Link href={item.link} passHref>
                         <div
-                            className={`truncate ${isExpand ? "" : isExpandOnHover ? "" : "w-0 h-0 opacity-0"
-                                }`}
+                            className={[
+                                "group m-0 flex cursor-pointer rounded-lg items-center justify-between h-12 py-0 pr-3 mb-1 focus:outline-none",
+                                recursive === 0 ? "pl-4" : recursive === 1 ? "pl-11" : "pl-16",
+                                "hover:bg-gray-300/20",
+                                classesActive
+                            ].join(" ")}
                         >
                             {item.title}
                         </div>
-                    </div>
-                    {"child" in item ? (
-                        <div
-                            className={`${isExpand ? "" : isExpandOnHover ? "" : "w-0 h-0 opacity-0"
-                                }`}
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
-                        </div>
-                    ) : (
-                        false
-                    )}
-                </a>
-                {"child" in item ? (
+                    </Link>
+                )}
+                {item.child && (
                     <ul
                         ref={(el) => (listRef.current[item.name] = el)}
                         className={[
-                            "overflow-hidden duration-300 ease-in-out",
+                            "transition-max-height overflow-hidden duration-300 ease-in-out",
                             isExpand ? "" : isExpandOnHover ? "" : "h-0"
                         ].join(" ")}
                         style={{ maxHeight: `${openedMenu[item.name]?.height || "0px"}` }}
                         key={item.name}
                     >
-                        {item.child.map((value: any, idx: number) =>
-                            generateMenu(value, idx, recursive + 1)
-                        )}
+                        {item.child.map((value, idx) => generateMenu(value, idx, recursive + 1))}
                     </ul>
-                ) : (
-                    false
                 )}
             </li>
         );
